c518a4fa4f7e66da2e3ff6603ec340352dab2559
Lodewijk Sioen <lodewijksioen@gmail.com>
2014-12-15 07:52:45 +0100

    External provider for HttpRequest

---
+++ src/ForWebforms.Tests/PartialControlTests.cs
@@ -0,0 +1,47 @@
+﻿using ExitStrategy.ForWebforms;
+using Moq;
+using Shouldly;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+
+namespace ForWebforms.Tests
+{
+    public class PartialControlTests
+    {
+        private Partial _control;
+
+        public PartialControlTests()
+        {
+            var httpContext = new Mock<HttpContextBase>();
+            var page = new MockPage();
+
+            httpContext.Setup(c => c.Items).Returns(new OrderedDictionary());
+            _control = new Partial();       
+            //page.SetupGet(p => p.Request.RequestContext).Returns(new RequestContext(httpContext.Object, new RouteData()));
+            page.Controls.Add(_control);     
+        }
+
+        public void TestRender()
+        {
+            var builder = new StringBuilder();
+            using(var writer = new HtmlTextWriter(new StringWriter(builder)))
+            {
+                _control.RenderControl(writer);
+            }
+
+            builder.ToString().ShouldNotBe(null);
+        }
+    }
+
+    public class MockPage : Page
+    {
+        public new HttpRequest Request
+        {
+            get { return new HttpRequest("", "", ""); }
+        }
+    }
+}
---
+++ src/ForWebforms/HttpRequestProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+namespace ExitStrategy.ForWebforms
+{
+    public static class HttpContextProvider
+    {
+        private static HttpContextBase _current;
+
+        public static HttpContextBase Current
+        {
+            get
+            {
+                return _current ?? HttpContext.Current.Request.RequestContext.HttpContext;
+            }
+        }
+
+        public static void SetHttpContext(HttpContextBase context)
+        {
+            _current = context;
+        }
+    }
+}

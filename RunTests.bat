9b1484555b3db98049b115439c520b515ab2f046
Lodewijk Sioen <lodewijksioen@gmail.com>
2014-12-19 00:03:55 +0100

    Unnececary comments

--- RunTests.bat
+++ RunTests.bat
@@ -1,12 +1,11 @@
 @ECHO OFF
 SETLOCAL
-REM You might prefer specific scripts for each tool. Makes the usage a bit easier. This is an example for NUnit.
  
 FOR /R %~dp0\src\packages %%G IN (Fixie.Console.exe) DO (
-IF EXIST %%G (
-SET TOOLPATH=%%G
-GOTO FOUND
-)
+    IF EXIST %%G (
+        SET TOOLPATH=%%G
+        GOTO FOUND
+    )
 )
 IF '%TOOLPATH%'=='' GOTO NOTFOUND
  

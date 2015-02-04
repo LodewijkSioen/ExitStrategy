b92e9fccb990d455a1453c499329236409bd844e
Lodewijk Sioen <lodewijksioen@gmail.com>
2015-02-04 12:04:14 +0100

    Debug info for appveyor

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,30 @@ var msbuild = require('gulp-msbuild');
 var exec = require('child_process').exec;
 var nuget = require('nuget-runner')({});
 
+var os = require('os');
+var path = require('path');
+var childProcess = require('child_process');
+
 gulp.task('default', ['nuget-restore', 'create-folders', 'build', 'test', 'nuget-pack']);
+
+gulp.task('debug-msbuild', function(){
+	if (!process.platform.match(/^win/)) {
+		console.log('Not running on windows!');
+	}
+
+	console.log('os.arch: ' + os.arch());
+
+	var program_files = os.arch() === 'x64' ? 'Program Files (x86)' : 'Program Files';
+	var msbuildPath = path.join('C:', program_files, 'MSBuild', '12.0', 'Bin/MSBuild.exe');
+  	console.log('MSBuild path: ' + msbuildPath);
+  	console.log('Normalized MSBuild path: ' + path.normalize(msbuildPath));
+
+  	console.log('Is MSBuild there: ' + fs.existsSync(msbuildPath));
+
+  	var cp = childProcess.exec('"' + msbuildPath + '"');
+  	cp.stdout.pipe(process.stdout);
+  	cp.stderr.pipe(process.stderr);
+});
 
 gulp.task('create-folders', function(){
 	return fs.mkdir('./artifacts');
@@ -18,7 +41,7 @@ gulp.task('build', ['nuget-restore'], function() {
 	return gulp
         .src('./src/ExitStrategy.sln')
         .pipe(msbuild({
-            toolsVersion: 12.0,
+            toolsVersion: '12.0',
             targets: ['Clean', 'Build'],
             errorOnFail: true,
             stdout: true

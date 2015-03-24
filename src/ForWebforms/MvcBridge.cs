4d61908143018f82415f1dae40a402eb13ee48ac
Lodewijk Sioen <lodewijksioen@gmail.com>
2015-03-24 20:47:26 +0100

    ViewBag was actually ViewData

--- src/ForWebforms/MvcBridge.cs
+++ src/ForWebforms/MvcBridge.cs
@@ -17,17 +17,17 @@ namespace ExitStrategy.ForWebforms
             return context.HttpContext.Items["ExitStrategy_ControllerContext"] as ControllerContext;
         }
 
-        public static ViewContext CreateViewContext(ControllerContext controllerContext, HtmlTextWriter output, ViewDataDictionary viewBag)
+        public static ViewContext CreateViewContext(ControllerContext controllerContext, HtmlTextWriter output, ViewDataDictionary viewData)
         {
             var view = new RazorView(controllerContext, "~/View", "", false, new string[0]);
-            return new ViewContext(controllerContext, view, viewBag, new TempDataDictionary(), output);
+            return new ViewContext(controllerContext, view, viewData, new TempDataDictionary(), output);
         }
 
-        public static HtmlHelper CreateHtmlHelper(RequestContext context, ViewDataDictionary viewBag, HtmlTextWriter output)
+        public static HtmlHelper CreateHtmlHelper(RequestContext context, ViewDataDictionary viewData, HtmlTextWriter output)
         {
             var controllerContext = CreateControllerContext(context);
-            var viewContext = CreateViewContext(controllerContext, output, viewBag);
-            return new HtmlHelper(viewContext, new WebformsViewDataContainer(viewBag));
+            var viewContext = CreateViewContext(controllerContext, output, viewData);
+            return new HtmlHelper(viewContext, new WebformsViewDataContainer(viewData));
         }
     }
 }

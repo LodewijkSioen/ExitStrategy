ae1f6acb1e111f2e67dfba3da8411abd3f315d36
Lodewijk Sioen <lodewijksioen@gmail.com>
2015-03-26 20:17:54 +0100

    Finding the parent DataBoundControl in a ListView

--- src/ForWebforms/ModelBinding/DataItemContainerBindingStrategy.cs
+++ src/ForWebforms/ModelBinding/DataItemContainerBindingStrategy.cs
@@ -25,21 +25,29 @@ namespace ExitStrategy.ForWebforms.ModelBinding
                 return new ModelDefinition(metaData, dataItem);
             }
 
-            var dataBoundControl = _control.DataItemContainer as DataBoundControl;
-            if (dataBoundControl != null)
+            var dataBoundControl = FindDataboundControl(_control);
+            
+            var typeName = dataBoundControl.ItemType;
+            if (string.IsNullOrEmpty(typeName))
             {
-                var typeName = dataBoundControl.ItemType;
-                if (string.IsNullOrEmpty(typeName))
-                {
-                    throw new InvalidOperationException(string.Format("Cannot determine the databinding type for control with id '{0}'. Please provide the correct type in the ItemType property of the control.", dataBoundControl.ID));
-                }
-
-                var type = BuildManager.GetType(typeName, true, false);
-                var metaData = ModelMetadataProviders.Current.GetMetadataForType(() => null, type);
-
-                return new ModelDefinition(metaData, null);
+                throw new InvalidOperationException(string.Format("Cannot determine the databinding type for control with id '{0}'. Please provide the correct type in the ItemType property of the control.", dataBoundControl.ID));
             }
-            return null;
+
+            var type = BuildManager.GetType(typeName, true, false);
+            var metaDataForItemType = ModelMetadataProviders.Current.GetMetadataForType(() => null, type);
+
+            return new ModelDefinition(metaDataForItemType, null);
+        }
+
+        public DataBoundControl FindDataboundControl(Control control)
+        {
+            var parent = control.DataKeysContainer;
+            var parentAsDataBoundControl = parent as DataBoundControl;
+            if (parentAsDataBoundControl == null)
+            {
+                throw new InvalidOperationException(string.Format("Cannot determine the DataBoundControl the control with ID '{0}' is nested in. Make sure the control is nested within the template of a DataBoundControl (eg. ListView, FormView,..)", _control.ID));
+            }
+            return parentAsDataBoundControl;
         }
     }
 }

677211664070682bfe4f13e8b94fab25edc6e3bb
Lodewijk Sioen <lodewijksioen@gmail.com>
2015-03-26 19:19:30 +0100

    BOOYA! Fixes #8

--- src/ForWebforms/ModelBinding/DataItemContainerBindingStrategy.cs
+++ src/ForWebforms/ModelBinding/DataItemContainerBindingStrategy.cs
@@ -9,7 +9,7 @@ namespace ExitStrategy.ForWebforms.ModelBinding
 {
     public class DataItemContainerBindingStrategy : IBindingStrategy
     {
-        MvcControl _control;
+        readonly MvcControl _control;
 
         public DataItemContainerBindingStrategy(MvcControl control)
         {
@@ -21,7 +21,7 @@ namespace ExitStrategy.ForWebforms.ModelBinding
             var dataItem = DataBinder.GetDataItem(_control.DataItemContainer);
             if (dataItem != null)
             {
-                var metaData = ModelMetadataProviders.Current.GetMetadataForProperty(() => null, dataItem.GetType(), _control.DataField);
+                var metaData = ModelMetadataProviders.Current.GetMetadataForType(() => null, dataItem.GetType());
                 return new ModelDefinition(metaData, dataItem);
             }
 
@@ -35,7 +35,7 @@ namespace ExitStrategy.ForWebforms.ModelBinding
                 }
 
                 var type = BuildManager.GetType(typeName, true, false);
-                var metaData = ModelMetadataProviders.Current.GetMetadataForProperty(() => null, type, _control.DataField);
+                var metaData = ModelMetadataProviders.Current.GetMetadataForType(() => null, type);
 
                 return new ModelDefinition(metaData, null);
             }
